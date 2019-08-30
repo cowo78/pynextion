940dabded3c414324579a2934fda5c113b389615
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2019-08-30 12:33:22 +0200

    Major rewrite. NexPage acts as a widget container. A base widget has been created, capable of enqueuing commands and reacting to commands completion/failure. The widget factory has been moved here.

--- pynextion/widgets.py
+++ pynextion/widgets.py
@@ -1,151 +1,232 @@
-from collections import OrderedDict
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from .objects import (
-    IWidget,
-    NexId,
-    PID_DEFAULT,
-    CID_DEFAULT
-)
+import collections
+import logging
+import typing
 
-from .interfaces import (
-    NxInterface,
-    IViewable,
-    IBooleanValued,
-    INumericalUnsignedValued,
-    INumericalSignedValued,
-    IStringValued,
-    IFontStyleable,
-    IColourable,
-    IPicturable,
-    ITouchable,
-    IWidthable,
-    IHeightable
-)
+from collections import ChainMap, OrderedDict
 
-from .exceptions import (
-    NexComponentException,
-    NexComponentNameException,
-    NexComponentIdException
-)
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+
+from .commands import CommandBase, PageCommand
+from .exceptions import NexComponentNameException, NexComponentIdException
+
+from .interfaces import NxInterface, IViewable, IBooleanValued, INumericalUnsignedValued, INumericalSignedValued, \
+    IStringValued, IFontStyleable, IColourable, IPicturable, ITouchable, IWidthable, IHeightable
 
 
-class NexButton(IWidget, IViewable, IStringValued, IFontStyleable, IColourable, ITouchable):
-    pass
+class NexWidget(QObject):
+    """ Base class for all widgets"""
 
+    enqueue_command = pyqtSignal(CommandBase)
+    """ Emitted whenever a command needs to be enqueued """
 
-class NexCheckbox(IWidget, IViewable, IBooleanValued, IColourable, ITouchable):
-    pass
+    command_failed = pyqtSignal(CommandBase)
+    """ Emitted whenever a command has failed """
 
+    REFRESH_VARIABLES = None  # type: typing.Union[None, typing.Iterable[str]]
+    ONETIME_REFRESH_VARIABLES = None  # type: typing.Union[None, typing.Iterable[str]]
 
-class NexCrop(IWidget, IViewable, IPicturable, ITouchable):
-    pass
+    def __init__(self, name: str, pid: int, cid: int = None, parent=None):
+        super().__init__(parent)
+        self._logger = logging.getLogger("pynextion.NexWidget")
+        self.name = name
+        self.pid = pid  # Page ID
+        self.cid = cid  # Component (widget) ID
+        self._properties_cache = {}  # type: typing.Dict[str, typing.Any]
+        self.commands = collections.deque()  # type: typing.Sequence[CommandBase]
 
+    def __str__(self) -> str:
+        return "{0.__class__.__name__} - Page ID {0.pid} - Component ID {0.cid} - Name {0.name}".format(self)
 
-class NexDualStateButton(IWidget, IViewable, IBooleanValued, IColourable, ITouchable):
-    pass
+    @pyqtSlot()
+    def _on_command_successful(self):
+        """ Dequeue command and disconnect handlers """
+        command = self.sender()
+        command.successful.disconnect()
+        command.failed.disconnect()
+        existing = self.commands.pop()
+        assert existing == command
 
+    @pyqtSlot()
+    def _on_command_failed(self):
+        """ Dequeue command and disconnect handlers. Relay command to command_failed signal. """
+        command = self.sender()
+        self.command_failed.emit(command)
+        command.successful.disconnect()
+        command.failed.disconnect()
+        existing = self.commands.pop()
+        assert existing == command
+        self._logger.error("Command %s failed on object %s with data event %s: {}", command, self, command.data_event)
 
-class NexGauge(IWidget, IViewable, INumericalUnsignedValued, IColourable, ITouchable):
-    pass
-
-
-class NexHotspot(IWidget, ITouchable):
-    pass
-
-
-class NexNumber(IWidget, IViewable, INumericalSignedValued, IFontStyleable, IColourable, ITouchable):
-    pass
-
-
-class NexPage(IWidget):
-    def __init__(self, nexserial, name, pid=PID_DEFAULT, cid=CID_DEFAULT):
-        self._nid = NexId(nexserial, name, pid, cid)
-        self.D_WIDGETS_BY_NAME = OrderedDict()
-        self.D_WIDGETS_BY_CID = OrderedDict()
-
-    def show(self):
-        return self._show_by_name()
-
-    def _show_by_id(self):
-        oid = self._nid.pid
-        return self._nid._nexserial.send("page %s" % oid)
-
-    def _show_by_name(self):
-        oid = self._nid.name
-        return self._nid._nexserial.send("page %s" % oid)
-
-    def ishown(self):
-        pid1 = self._nid._nexserial.current_page
-        pid2 = self.pid
-        return pid1 == pid2
-
-    def hook_widget(self, widget_type, name, cid=CID_DEFAULT):
-        pid = self._nid.pid
-        if name in self.D_WIDGETS_BY_NAME.keys():
-            raise NexComponentNameException("name (%s) must be unique" % name)
-        if cid in self.D_WIDGETS_BY_CID.keys():
-            raise NexComponentIdException("cid (%s) must be unique" % cid)
-        widget = widget_type(self._nid._nexserial, name, pid=pid, cid=cid)
-        self.D_WIDGETS_BY_NAME[name] = widget
-        self.D_WIDGETS_BY_CID[cid] = widget
-        return widget
-
-    def widget(self, name=None, cid=None):
-        if name is not None and cid is None:
-            return self.D_WIDGETS_BY_NAME[name]
-        elif name is None and cid is not None:
-            return self.D_WIDGETS_BY_CID[cid]
-        elif name is not None and cid is not None:
-            raise NexComponentException("name and cid shouldn't be defined both")
-        else:
-            raise NexComponentException("name or cid should be defined")
+    def send_command(self, command: CommandBase):
+        """ Enqueue a command to be executed """
+        self.commands.appendleft(command)
+        command.failed.connect(self._on_command_failed)
+        command.successful.connect(self._on_command_successful)
+        self.enqueue_command.emit(command)
 
     def to_dict(self):
         return {
-            "pid": self._nid.pid,
-            "name": self._nid.name,
-            "components": [widget.to_dict() for widget in self.widgets]
+            "pid": self.pid,
+            "cid": self.cid,
+            "name": self.name,
+            "type": self.__class__.__name__.strip("Nex")
         }
+
+
+class NexButton(NexWidget, IViewable, IStringValued, IFontStyleable, IColourable, ITouchable):
+    ONETIME_REFRESH_VARIABLES = ("txt",)
+
+
+class NexCheckbox(NexWidget, IViewable, IBooleanValued, IColourable, ITouchable):
+    pass
+
+
+class NexCrop(NexWidget, IViewable, IPicturable, ITouchable):
+    pass
+
+
+class NexDualStateButton(NexWidget, IViewable, IBooleanValued, IColourable, ITouchable):
+    pass
+
+
+class NexGauge(NexWidget, IViewable, INumericalUnsignedValued, IColourable, ITouchable):
+    pass
+
+
+class NexHotspot(NexWidget, ITouchable):
+    pass
+
+
+class NexNumber(NexWidget, IViewable, INumericalSignedValued, IFontStyleable, IColourable, ITouchable):
+    pass
+
+
+class NexPage(NexWidget):
+    def __init__(self, name, pid=None, **kwargs_ignored):
+        """ Create a new Page. kwargs are ignored to be compatible with factory function """
+        super().__init__(name, pid)
+        self.D_WIDGETS_BY_NAME = OrderedDict()
+        self.D_WIDGETS_BY_CID = OrderedDict()
+        self._widgets_by_name_or_id = ChainMap(self.D_WIDGETS_BY_NAME, self.D_WIDGETS_BY_CID)
+        self._page_switch_in_progress = False
 
     @property
     def widgets(self):
-        for name, widget in self.D_WIDGETS_BY_NAME.items():
-            yield widget
+        return self.D_WIDGETS_BY_NAME.values()
+
+    def widget(self, name_or_id: typing.Union[str, int]):
+        """ Access a widget by either name or ID"""
+        return self._widgets_by_name_or_id[name_or_id]
+
+    __getitem__ = widget
+
+    @pyqtSlot()
+    def refresh(self):
+        """ Refresh all (hooked) widgets in current page """
+        if not self._page_switch_in_progress:
+            [widget.refresh() for widget in self.widgets]
+
+    @pyqtSlot()
+    def onetime_refresh(self):
+        if not self._page_switch_in_progress:
+            [widget.onetime_refresh() for widget in self.widgets]
+
+    def show(self):
+        """ Bring the current page to foreground.
+            **DO NOT** call this method directly, use select_page() on parent NexDevice instead
+            so it can keep track of current page
+        """
+        self._page_switch_in_progress = True
+        target = self.name if self.pid is None else self.pid
+        self.send_command(PageCommand(target))
+
+    def hook_widget(self, widget_type: str, name: str, cid=None) -> NexWidget:
+        """ Hook and return a new widget of the specified type/name/ID to the current page """
+        pid = self.pid
+
+        if name in self.D_WIDGETS_BY_NAME:
+            raise NexComponentNameException("Widget name (%s) must be unique" % name)
+
+        if cid in self.D_WIDGETS_BY_CID:
+            raise NexComponentIdException("Widget ID (%s) must be unique" % cid)
+
+        widget = WidgetFactory.create(widget_type, name, pid, cid)
+        self.D_WIDGETS_BY_NAME[name] = widget
+        if cid is not None:
+            self.D_WIDGETS_BY_CID[cid] = widget
+        widget.enqueue_command.connect(self.enqueue_command)
+        self._logger.debug("Hooked new widget %s", name)
+        return widget
+
+    def hook_widgets(self, widget_data: typing.Iterable[typing.Tuple[str, str, int]]) -> typing.List[NexWidget]:
+        """ Hook and return some widgets
+            :param widget_data: iterable of (widget type, widget name, widget id)
+        """
+        return [self.hook_widget(widget_type, name, cid) for widget_type, name, cid in widget_data]
+
+    @pyqtSlot()
+    def _on_command_successful(self):
+        command = self.sender()
+        command.successful.disconnect()
+        command.failed.disconnect()
+        existing = self.commands.pop()
+        assert existing == command
+        if isinstance(command, PageCommand):
+            self._page_switch_in_progress = False
+
+    def to_dict(self):
+        return {
+            "pid": self.pid,
+            "name": self.name,
+            "components": [widget.to_dict() for widget in self.widgets]
+        }
 
 
-class NexPicture(IWidget, IViewable, IPicturable):
+class NexPicture(NexWidget, IViewable, IPicturable):
     pass
 
 
-class NexProgressBar(IWidget, IViewable, INumericalUnsignedValued, IColourable, ITouchable):
+class NexProgressBar(NexWidget, IViewable, INumericalUnsignedValued, IColourable, ITouchable):
+    ONETIME_REFRESH_VARIABLES = ("val",)
+
+    def increase(self, amount: int = 1):
+        self
+        self.value += amount
+
+    def decrease(self, amount: int = 1):
+        self.value -= amount
+
+
+class NexQRcode(NexWidget, IViewable, IStringValued):
     pass
 
 
-class NexQRcode(IWidget, IViewable, IStringValued):
+class NexRadio(NexWidget, IViewable, IBooleanValued, IColourable, ITouchable):
     pass
 
 
-class NexRadio(IWidget, IViewable, IBooleanValued, IColourable, ITouchable):
+class NexScrollText(NexWidget, IViewable, IStringValued, IFontStyleable, IColourable, ITouchable):
     pass
 
 
-class NexScrollText(IWidget, IViewable, IStringValued, IFontStyleable, IColourable, ITouchable):
-    pass
-
-
-class NexSliderCursor(IWidget, IWidthable, IHeightable):
+class NexSliderCursor(NexWidget, IWidthable, IHeightable):
     def __init__(self, nid):
         self._nid = nid
 
 
-class NexSlider(IWidget, IViewable, INumericalUnsignedValued, IColourable, ITouchable):
+class NexSlider(NexWidget, IViewable, INumericalUnsignedValued, IColourable, ITouchable):
+    ONETIME_REFRESH_VARIABLES = ("val",)
+
     @property
     def cursor(self):
         return NexSliderCursor(self._nid)
 
 
-class NexText(IWidget, IViewable, IStringValued, IFontStyleable, IColourable, ITouchable):
-    pass
+class NexText(NexWidget, IViewable, IStringValued, IFontStyleable, IColourable, ITouchable):
+    ONETIME_REFRESH_VARIABLES = ("txt",)
 
 
 class NexWaveformChannel:
@@ -167,7 +248,7 @@ class NexWaveformChannel:
             return nexserial.read_all()
         else:
             if value < 0 or value > 255:
-                raise(Exception("value must be in 0-255 range"))
+                raise (Exception("value must be in 0-255 range"))
             cmd = "add %s,%s,%s" % (cid, chid, value)
             return nexserial.send(cmd)
 
@@ -180,7 +261,7 @@ class NexWaveformChannels:
         return NexWaveformChannel(self._nid, id)
 
 
-class NexWaveformGrid(IWidget, NxInterface):
+class NexWaveformGrid(NexWidget, NxInterface):
     def __init__(self, nid):
         self._nid = nid
 
@@ -209,7 +290,7 @@ class NexWaveformGrid(IWidget, NxInterface):
         self._set_nex_number_property("gdc", new_color.value)
 
 
-class NexWaveform(IWidget, IViewable, IColourable, ITouchable):
+class NexWaveform(NexWidget, IViewable, IColourable, ITouchable):
     @property
     def grid(self):
         return NexWaveformGrid(self._nid)
@@ -217,3 +298,44 @@ class NexWaveform(IWidget, IViewable, IColourable, ITouchable):
     @property
     def channels(self):
         return NexWaveformChannels(self._nid)
+
+
+class WidgetFactory:
+    D_FACTORY = {
+        "button": NexButton,
+        "checkbox": NexCheckbox,
+        "crop": NexCrop,
+        "dualstatebutton": NexDualStateButton,
+        "gauge": NexGauge,
+        "hotspot": NexHotspot,
+        "number": NexNumber,
+        "page": NexPage,
+        "picture": NexPicture,
+        "progressbar": NexProgressBar,
+        "qrcode": NexQRcode,
+        "radio": NexRadio,
+        "scrolltext": NexScrollText,
+        "slider": NexSlider,
+        "text": NexText,
+        "waveform": NexWaveform
+    }
+
+    @classmethod
+    def type(cls, typ: str):
+        return cls.D_FACTORY[typ.lower()]
+
+    @classmethod
+    def create(cls, typ: typing.Union[str, typing.Type], name: str, pid=None, cid=None) -> NexWidget:
+        """ Create a new instance of specified widget
+            :param typ: Widget type as string (defined in D_FACTORY) or widget class
+            :param name: Widget name
+            :param pid: Page ID
+            :param cid: Widget ID
+        """
+        if isinstance(typ, str):
+            widget = cls.D_FACTORY[typ.lower()](name, pid=pid, cid=cid)
+        else:
+            # Assume it is a type
+            widget = typ(name, pid, cid)
+
+        return widget
