b889a0f62fd910169e7d61f662b34bdc68ea1b63
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2019-08-30 12:27:45 +0200

    Major rewrite. The serial abstractor has no knowledge of commands being sent/received and only knows how to segment incoming data into events to be handled by a layer above.

--- pynextion/hardware.py
+++ pynextion/hardware.py
@@ -1,14 +1,12 @@
-import time
-from .constants import Return
-from .events import (
-    MsgEvent,
-    StringHeadEvent,
-    NumberHeadEvent,
-    CurrentPageIDHeadEvent
-)
-from .hook import (
-    NexComponents
-)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import collections
+import queue
+import threading
+import typing
+
+from .constants import S_END_OF_CMD
 
 try:
     import serial
@@ -17,114 +15,89 @@ except ImportError:
     _HAS_PYSERIAL = False
 
 
-A_END_OF_CMD = [0xff, 0xff, 0xff]
-S_END_OF_CMD = bytearray(A_END_OF_CMD)
+class AbstractSerialNex(object):
+    INCOMING_BUFFER_SIZE = 1024  # Seems the Nextion buffer size, mentioned in official docs
+    MIN_SIZE_READ = len(S_END_OF_CMD) + 1  # Minimum return data size
+    TERMINATOR_SIZE = len(S_END_OF_CMD)
 
-
-def _format_cmd(cmd):
-    return bytearray(cmd, encoding="ASCII") + S_END_OF_CMD
-
-
-class AbstractSerialNex:
     def __init__(self):
-        self.components = NexComponents(self)
+        super().__init__()
+        self._port_mutex = threading.Lock()
+        # Queue of event objects
+        self._events = queue.Queue()
+        # Incoming serial buffer
+        self._buffer = bytearray(self.INCOMING_BUFFER_SIZE)
+        self._events_queue = collections.deque()  # type: typing.Sequence[bytearray]
 
-    def write(self, cmd):
-        print("cmd --> %s" % cmd)
-        cmd = _format_cmd(cmd)
-        print("cmd --> %s" % cmd)
-        return self.sp.write(cmd)
+    def write(self, data: bytes) -> int:
+        """ Raw write access to underlying transport. Threadsafe.
+        :returns: Number of bytes written
+        """
+        with self._port_mutex:
+            nbytes = self.sp.write(data)
 
-    def init(self):
-        # mode = Return.Mode.NO_RETURN
-        # mode = Return.Mode.SUCCESS_ONLY  # production setting
-        # mode = Return.Mode.FAIL_ONLY  # default screen setting
-        mode = Return.Mode.ALWAYS  # for debug
-        ret1 = self.set_cmd_response_mode(mode)
-        ret2 = self.send("page 0")
-        if mode in [Return.Mode.ALWAYS, Return.Mode.SUCCESS_ONLY]:
-            return ret1.issuccess() and ret2.issuccess()
-        elif mode in [Return.Mode.FAIL_ONLY, Return.Mode.NO_RETURN]:
-            return ret1.isempty() and ret2.isempty()
-        else:
-            raise NotImplementedError("Undefined mode %s" % mode)
+        return nbytes
+    send = write
 
-    def set_cmd_response_mode(self, mode):
-        assert isinstance(mode, Return.Mode), "Mode must be a Return.Mode enum"
-        cmd = "bkcmd=%d" % mode.value
-        return self.send(cmd)
+    def read_all(self) -> bytes:
+        """ Read all buffered data. Threadsafe. """
+        with self._port_mutex:
+            data = self.sp.read_all()
 
-    def reset(self):
-        cmd = "rest"
-        # ret = self.send(cmd)
-        self.write(cmd)
-        time.sleep(1)
-        msg = self.read_all()
-        assert msg == b'\x00\x00\x00\xff\xff\xff\x88\xff\xff\xff', "rest returned %s" % msg
-        return True
+        return data
 
-    def read_all(self):
-        return self.sp.read_all()
+    def read_next(self) -> bytes:
+        """ Read next message. Threadsafe. May return an empty array is no event is available. """
+        # At some point (along with editor 0.58) the Nextion firmware changed and now it returns
+        # an "instruction successful" everytime, even after a string or numeric data event
+        if self._events_queue:
+            return self._events_queue.pop()
 
-    def send(self, cmd):
-        self.write(cmd)
-        time.sleep(0.1)
-        msg = self.read_all()
-        print("msg <-- %s" % msg)
-        evt = MsgEvent.parse(msg)
-        print("msg <-- %s" % evt)
-        return evt
+        buffer_size = 0
+        with self._port_mutex:
+            # Reading one byte at a time is of course inefficient, so serial.read_until is not the best option
+            # We know the minimal read should be 4 chars (i.e. Invalid Instruction) and must be prepared to
+            # partial command reads since we have no guarantee that we will always have complete commands in the buffer
+            if self.sp.in_waiting < self.MIN_SIZE_READ:
+                # Partial event, unlikely at this point
+                return b''
 
-    def get_nex_string_command(self, cmd):
-        evt = self.send(cmd)
-        assert isinstance(evt, StringHeadEvent)
-        return evt.value
+            # Read bulk of data
+            chunk = self.sp.read(self.sp.in_waiting)
+            self._buffer[buffer_size:buffer_size+len(chunk)] = chunk
+            buffer_size = len(chunk)
+            while self._buffer[buffer_size - self.TERMINATOR_SIZE:buffer_size] != S_END_OF_CMD:
+                # Trickle until end of event
+                chunk = self.sp.read(1)
+                self._buffer[buffer_size:buffer_size+1] = chunk
+                buffer_size += 1
 
-    def set_nex_string_command(self, cmd):
-        return self.send(cmd)
+        # Finished reading and we are sure we have complete event(s), now split into single events
+        start = 0
+        while True:
+            pos = self._buffer.find(S_END_OF_CMD, start, buffer_size)
+            if pos == -1:
+                break
 
-    def get_nex_number_command(self, cmd, signed, bit_size):
-        evt = self.send(cmd)
-        assert isinstance(evt, NumberHeadEvent)
-        if signed:
-            return evt.signed_value
-        else:
-            return evt.value
+            self._events_queue.appendleft(self._buffer[start:pos+self.TERMINATOR_SIZE])
+            start = pos + self.TERMINATOR_SIZE
 
-    def set_nex_number_command(self, cmd):
-        return self.send(cmd)
-
-    def get_nex_bool_command(self, cmd):
-        evt = self.send(cmd)
-        assert isinstance(evt, NumberHeadEvent)
-        return bool(evt.value)
-
-    def set_nex_bool_command(self, cmd):
-        return self.send(cmd)
-
-    @property
-    def current_page(self):
-        cmd = "sendme"
-        evt = self.send(cmd)
-        assert isinstance(evt, CurrentPageIDHeadEvent)
-        return evt.pid
+        return self._events_queue.pop()
 
     def close(self):
         return self.sp.close()
 
-    def poll(self):
-        print("poll")
-        data = self.read_all()
-        print("data: %s" % data)
-
 
 if _HAS_PYSERIAL:
     class PySerialNex(AbstractSerialNex):
-        def __init__(self, *args, **kwargs):
+        def __init__(self, port_or_url: str, *args, **kwargs):
             super().__init__()
-            self.sp = serial.Serial(*args, **kwargs)
+            self.sp = serial.serial_for_url(port_or_url, *args, **kwargs)
+            self.sp.reset_input_buffer()
+            self.sp.reset_output_buffer()
 
 
+# TODO: rotten
 class NexSerialMock(AbstractSerialNex):
     def __init__(self, *args, **kwargs):
         super().__init__()
